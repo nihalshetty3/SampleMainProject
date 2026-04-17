07f39678f339eb0a7322f6a252a070ddcd39fc60
Nihal Shetty <nihalhshetty30@gmail.com>
2026-04-17 19:57:32 +0530

    test commit for github webhook

--- project/t.java
+++ project/t.java
@@ -7,5 +7,6 @@ public class t{
     public static void main(String args[])
     {
         System.out.println("Test webhook trigger for Github action");
+        System.out.println("Latest trigger check");
     }
 }
